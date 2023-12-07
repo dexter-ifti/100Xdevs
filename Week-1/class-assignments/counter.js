636da905756bd7a71e57d0fd4c64015907c2c9e4
Taha <tiftikhar@student.iul.ac.in>
2023-12-07 07:43:34 +0000

    add commnets

--- Week-1/class-assignments/counter.js
+++ Week-1/class-assignments/counter.js
@@ -1,19 +1,34 @@
+// Initialize a counter variable to 30
 let counter = 30;
+
+// Define a function named 'print'
 function print() {
+    // If the counter is less than 0, exit the function
     if (counter < 0) {
         return;
     }
+    // Log the current counter value and then decrement it
     console.log(counter--);
 }
-setInterval(print, 100);
 
+// Call the 'print' function every 100 milliseconds
+setInterval(print, 1000);
+
+// Seconf Method - 
+// Define a second function named 'print2' 
 function print2() {
+    // If the counter is less than 0, exit the function
     if(counter < 0) return;
+    // Log the current counter value and then decrement it
     console.log(counter--);
+    // Call the 'print2' function after 100 milliseconds
     setTimeout(print2, 100);
 }
+
+// either call this 
 // setTimeout(print2, 100);
-// print2();
+// or this
+print2();
 
 /*
 * for(let i = 0; i < 30; i++){
