99c0a24ac910faebdaaaaa375618923662506ab1
Taha <tiftikhar@student.iul.ac.in>
2023-12-07 06:59:59 +0000

    Added Week 0 and Week 1 content details in the README file

---
+++ Week-1/main.js
@@ -0,0 +1,46 @@
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// for(let i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 == 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// const users = 
+// [
+//     {
+//         fn : "taha",
+//         gender : "M"
+//     }, 
+//     {
+//         fn : "ifti",
+//         gender : "M"
+//     },
+//     {
+//         fn : "priya",
+//         gender : "F"
+//     }
+// ]
+// for(let i = 0; i < users.length; i++){
+//     if (users[i]["gender"] == "F") {
+//         console.log(users[i]["fn"]);
+//     }
+// }
+// let sum = 0;
+// for(let i = 0; i < 1000000000000; i++){
+//     sum += i;
+// }
+// console.log(sum);
+// meri core dag gyi 🔥🔥🔥🔥🔥
+
+function sum(a, b) {
+    return a + b;
+}
+function print(result) {
+    console.log(result);
+}
+
+// print(sum(1,2));
+
+setTimeout(() => {
+    console.log("hello");
+}, 10000);
