330a4810c4aa3fa6110019458bebc7d6cae288ed
Taha <tiftikhar@student.iul.ac.in>
2023-12-12 16:22:37 +0000

    Add promise chain function to calculate time

---
+++ Week-2/assignments/01-async-js/hard(promises)/4-promise-chain.js
@@ -0,0 +1,41 @@
+/*
+ * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
+ * Write a function that sequentially calls all 3 of these functions in order.
+ * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
+ * Compare it with the results from 3-promise-all.js
+ */
+
+function wait1(t) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, t*1000)
+    });
+}
+
+function wait2(t) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, t*1000)
+    });
+}
+
+function wait3(t) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, t*1000)
+    });
+}
+
+async function calculateTime(t1, t2, t3) {
+    const start = Date.now();
+    await wait1(t1);
+    await wait2(t2);
+    await wait3(t3);
+    const end = Date.now();
+    return end - start;
+}
+
+module.exports = calculateTime;
