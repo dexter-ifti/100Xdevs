8e746aed31c1dd9a3d160f8895697e16b6f04584
Taha <tiftikhar@student.iul.ac.in>
2023-12-15 05:03:22 +0000

    Refactor file cleaner script to read and write to the same file

--- Week-2/assignments/01-async-js/medium/01-file-cleaner.js
+++ Week-2/assignments/01-async-js/medium/01-file-cleaner.js
@@ -12,10 +12,18 @@
 
 const fs = require('fs');
 // let str;
-fs.readFile('a.txt', 'utf8', (err, data) => {
+const path = "/workspaces/100Xdevs/Week-2/assignments/01-async-js/medium/a.txt"
+// Reading from a file
+fs.readFile(path, 'utf8', (err, data) => {
   if (err) throw err;
   let str = data;
   console.log(str);
   str = str.replace(/\s+/g,' ').trim();
+  // Writting again to the same file
+  fs.writeFile(path, str, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
   console.log(str);
 });
+
