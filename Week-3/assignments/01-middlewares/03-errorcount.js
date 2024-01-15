9aa4b3b5ebc0ae61068000e58ab8fe39981a1ed2
Taha <tiftikhar@student.iul.ac.in>
2024-01-15 13:05:12 +0000

    Add error handling middleware

--- Week-3/assignments/01-middlewares/03-errorcount.js
+++ Week-3/assignments/01-middlewares/03-errorcount.js
@@ -23,4 +23,8 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use((err, req, res, next) => {
+  res.status(404).send({});
+  errorCount++;
+})
 module.exports = app;
