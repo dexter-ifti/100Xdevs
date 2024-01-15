40eb40d2489cd348d78c0d71be9ea337ddf4483a
Taha <tiftikhar@student.iul.ac.in>
2024-01-15 13:05:06 +0000

    Add rate limiting middleware

--- Week-3/assignments/01-middlewares/02-ratelimitter.js
+++ Week-3/assignments/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,21 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use( (req, res, next) => {
+  const userId = req.headers['user-id'];
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("You Exceeded the rate limit");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
