97faa160f664a80a6cd04402a9e3480de81bbfaf
Taha <tiftikhar@student.iul.ac.in>
2024-01-21 17:49:44 +0000

    Add loops examples

---
+++ 00-chai-aur-js/04-iterations/01-loops.js
@@ -0,0 +1,18 @@
+// for loop
+for(let i = 0; i < 10; i++){
+    console.log(i+1);
+}
+
+
+// while loop
+
+let i = 0; 
+while (i < 10) {
+    console.log(i+1);
+}
+
+// do-while loop
+i = 0;
+do {
+    console.log(i+1);
+} while (i < 10);
