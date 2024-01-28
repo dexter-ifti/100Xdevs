a421b66cbdf890d4eb0982247fed7ccf5678a17e
Taha <tiftikhar@student.iul.ac.in>
2024-01-28 14:22:11 +0000

    Add color changer functionality to index.html and chaiaurcode.js

---
+++ 00-chai-aur-js/06-projects/01-color-changer/chaiaurcode.js
@@ -0,0 +1,25 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+button.forEach((button) => {
+    console.log(button);
+    button.addEventListener('click', (e)=> {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
