cef3330544d54feeba68cfe44974b1ff36af7d7c
Taha <tiftikhar@student.iul.ac.in>
2024-03-06 01:43:21 +0000

    basic react app from vite

---
+++ 04-react/01-vite-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello dexter ifti</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 04-react/01-vite-react-app/src/chai.jsx
@@ -0,0 +1,5 @@
+function Chai() {
+    return <h3>chai is ready</h3>
+}
+
+export default Chai;
