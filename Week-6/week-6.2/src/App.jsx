634d214fcad34db1aefcc9acf0f8fdd70d834760
dexter-ifti <tiftikhar@student.iul.ac.in>
2024-03-30 23:54:44 +0530

    Refactor component imports and remove commented code

--- Week-6/week-6.2/src/App.jsx
+++ Week-6/week-6.2/src/App.jsx
@@ -5,11 +5,10 @@ function App() {
 
   return (
     <>
-    {/* <App1></App1>
+    <App1></App1>
     <App2></App2>
     <App3></App3> 
     <App4></App4>
-    */}
     <App5></App5>
     </>
   )
---
+++ Week-6/week-6.2/src/components/index.js
@@ -0,0 +1,7 @@
+import App1 from './App1'
+import App2 from './App2'
+import App3 from './App3'
+import App4 from './App4'
+import App5 from './App5'
+
+export  {App1, App2, App3, App4, App5}
