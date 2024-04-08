c17818329b7e4207f7b069a8878e41f2e220abd9
Taha <tiftikhar@student.iul.ac.in>
2024-04-08 04:14:37 +0000

    Add Landing and Dashboard components

---
+++ Week-7/week7.2/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+        Dashboard Page
+    </div>
+  )
+}
+
+export default Dashboard    
---
+++ Week-7/week7.2/src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        Landing Page
+    </div>
+  )
+}
+
+export default Landing
