8e186c8f905711e446154ed15f27750206974300
Taha <tiftikhar@student.iul.ac.in>
2024-04-08 06:03:54 +0000

    Refactor App.jsx to use App5 component instead of individual App1, App2, App3, and App4 components

--- Week-7/week7.1/src/App.jsx
+++ Week-7/week7.1/src/App.jsx
@@ -1,39 +1,20 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
-const Landing = React.lazy(() => import("./components/Landing"));
-const Dashboard = React.lazy(() => import("./components/Dashboard"));
+import {App1, App2, App3, App4, App5} from "./components";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <AppBar />
-        <Routes>
-          <Route path="/dashboard" element={<Suspense fallback={"loading..."}>
-          <Dashboard /></Suspense>
-          }></Route>
-          <Route path="/" element={<Suspense fallback={"loading..."}>
-          <Landing /></Suspense>
-          }></Route>
-        </Routes>
-      </BrowserRouter>
+      {/* 
+      <App1/>
+      <App2/> 
+      <App3/>
+      <App4/>
+      */}
+      <App5/>
     </div>
   );
 }
 
-function AppBar() {
-  const navigate = useNavigate()
-  return (
-    <div>
-      <div style={{ background: "red" }}>Fixed Top Bar</div>
-      <div>
-        {/* <button onClick={() => window.location.href = "/dashboard"}>Dashboard Page</button> */}
-        <button onClick={() => (navigate("/"))}>Landing Page</button>
-        <button onClick={() => (navigate("/dashboard"))}>Dashboard Page</button>
-      </div>
-    </div>
-  );
-}
 
 export default App;
--- Week-7/week7.1/src/components/index.js
+++ Week-7/week7.1/src/components/index.js
@@ -1,4 +1,9 @@
 import Dashboard from "./Dashboard";
 import Landing from "./Landing";
+import App1 from "./App1";
+import App2 from "./App2";
+import App3 from "./App3";
+import App4 from "./App4";
+import App5 from "./App5";
 
-export { Dashboard, Landing };
+export { Dashboard, Landing, App1, App2, App3, App4, App5};
