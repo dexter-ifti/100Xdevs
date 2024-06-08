aef9d9ae7656f58e21f391f9eae0f0203437779d
Taha <tiftikhar@student.iul.ac.in>
2024-06-08 18:26:48 +0000

    feat: Add App component with App1 to App7 components
    This commit adds the App component to the project, which renders the App1 to App7 components. This allows for the display of multiple components in the application. The addition of these components enhances the functionality and user experience of the application.

---
+++ Week-9/week-9.1/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import App1 from './components/App1'
+import App2 from './components/App2'
+import App3 from './components/App3'
+import App4 from './components/App4'
+import App5 from './components/App5'
+import App6 from './components/App6'
+import App7 from './components/App7'
+
+function App() {
+  return (
+    <>
+      <App1/>
+      <App2/> 
+      <App3/>
+      <App4/>
+      <App5/>
+      <App6/>
+      <App7/>
+    </>
+  )
+}
+
+export default App
