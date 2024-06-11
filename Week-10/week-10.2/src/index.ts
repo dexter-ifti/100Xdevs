7caa4385fca2b35a673346527ec3956076628587
Taha <tiftikhar@student.iul.ac.in>
2024-06-11 18:20:04 +0000

    feat: Add function to insert multiple users into PostgreSQL database

---
+++ Week-10/week-10.2/src/index.ts
@@ -0,0 +1,28 @@
+import {PrismaClient} from  "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function insertUser(username:string, password: string, firstName: string, lastName: string, email: string, phone: string) {
+    const user = await prisma.user.create({
+        data: {
+            email : username,
+            password,
+            firstName,
+            lastName,
+        },
+        select: {
+            id: true,
+            password : true
+        },
+    })
+    console.log(user);
+    
+}
+
+async function main() {
+    await insertUser("user1", "password1", "first1", "last1", "email1", "phone1")
+    await insertUser("user2", "password2", "first2", "last2", "email2", "phone2")
+    await insertUser("user3", "password3", "first3", "last3", "email3", "phone3")
+    await insertUser("user4", "password4", "first4", "last4", "email4", "phone4")
+    await insertUser("user5", "password5", "first5", "last5", "email5", "phone5")
+}
