8e88b3de0c7e99862b6bcf80b0eddc0c56eba989
Taha <tiftikhar@student.iul.ac.in>
2024-06-16 10:56:31 +0000

    chore: Update user and todo database functions
    This commit updates the user and todo database functions in the `src/db/user.ts` and `src/db/todo.ts` files. It includes changes to the `createUser`, `getUser`, `createTodo`, `updateTodo`, and `getTodos` functions to use the Prisma client for database operations. These changes enhance the codebase by improving the efficiency and reliability of database interactions in the application.

--- Week-10/Assignments/2-prisma-simple/src/db/todo.ts
+++ Week-10/Assignments/2-prisma-simple/src/db/todo.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { todo } from 'node:test';
 
 const prisma = new PrismaClient();
 
@@ -14,6 +15,20 @@ const prisma = new PrismaClient();
  */
 export async function createTodo(userId: number, title: string, description: string) {
     
+    const res = await prisma.todo.create({
+        data : {
+            userId, 
+            title,
+            description
+        },
+        select:{
+            title: true,
+            description: true,
+            done: true,
+            id: true
+        }
+    })
+    return res;
 }
 /*
  * mark done as true for this specific todo.
@@ -26,7 +41,22 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
+    const res = await prisma.todo.update({
+        where:{
+            id: todoId
+        },
+        data:{
+            done : true
+        },
+        select:{
+            title: true,
+            description: true,
+            done: true,
+            id: true
+        }
 
+    })
+    return res;
 }
 
 /*
@@ -40,5 +70,16 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+    const res = await prisma.todo.findMany({
+        where:{
+            userId
+        },
+        select:{
+            title: true,
+            description: true,
+            done: true,
+            id: true
+        }
+    })
+    return res;
 }
--- Week-10/Assignments/2-prisma-simple/src/db/user.ts
+++ Week-10/Assignments/2-prisma-simple/src/db/user.ts
@@ -13,7 +13,20 @@ const prisma = new PrismaClient();
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    const res = await prisma.user.create({
+        data:{
+            username,
+            password,
+            name
+        },
+        select:{
+            id : true,
+            username: true,
+            password: true,
+            name: true
+        }
+    })
+    return res;
 }
 
 /*
@@ -25,5 +38,16 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    const res = await prisma.user.findUnique({
+        where:{
+            id: userId
+        },
+        select:{
+            id : true,
+            username: true,
+            password: true,
+            name: true
+        }
+    })
+    return res;
 }
