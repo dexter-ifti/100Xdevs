136387efc8e15f171adb72a2d7d1dc9bc32a05d5
Taha <tiftikhar@student.iul.ac.in>
2024-06-23 15:21:36 +0000

    feat: Add createAddressTable function
    This commit adds the createAddressTable function to the create_index.ts file. The function creates a new table called addresses in the database, with columns for id, user_id, city, country, street, pincode, and created_at. It also adds a foreign key constraint to reference the users table. This addition enhances the functionality of the code by allowing the storage of address information for users.

--- Week-10/week-10.1/src/create_index.ts
+++ Week-10/week-10.1/src/create_index.ts
@@ -19,4 +19,22 @@ async function createUserTable() {
   console.log(result);
 }
 
+async function createAddressTable() {
+  await client.connect();
+  const res = await client.query(`
+      CREATE TABLE addresses (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL,
+      city VARCHAR(100) NOT NULL,
+      country VARCHAR(100) NOT NULL,
+      street VARCHAR(255) NOT NULL,
+      pincode VARCHAR(20),
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+      );
+  `);
+  console.log(res);
+}
+
 createUserTable();
+createAddressTable();
