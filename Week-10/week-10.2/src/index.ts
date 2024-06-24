4a3a3cb4dee6ce9111cdfbc6380bf1d99264b418
Taha <tiftikhar@student.iul.ac.in>
2024-06-24 11:02:48 +0000

    feat: Add updateUser function
    This commit adds the updateUser function to the index.ts file. The function allows updating a user's first name and last name in the database by using the Prisma ORM. This addition enhances the functionality of the code by providing the ability to update user information.

--- Week-10/week-10.2/src/index.ts
+++ Week-10/week-10.2/src/index.ts
@@ -19,10 +19,60 @@ async function insertUser(username:string, password: string, firstName: string, 
     
 }
 
-async function main() {
-    await insertUser("user1", "password1", "first1", "last1", "email1", "phone1")
-    await insertUser("user2", "password2", "first2", "last2", "email2", "phone2")
-    await insertUser("user3", "password3", "first3", "last3", "email3", "phone3")
-    await insertUser("user4", "password4", "first4", "last4", "email4", "phone4")
-    await insertUser("user5", "password5", "first5", "last5", "email5", "phone5")
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
 }
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams){
+    const user = await prisma.user.update({
+        where : {
+            email: username
+        },
+        data: {
+            firstName,
+            lastName
+        },
+        select : {
+            id: true,
+            firstName: true,
+        }
+    })
+    console.log(user);
+}
+
+
+async function getUser(username:string) {
+    const user = await prisma.user.findMany({
+        where: {
+            email: username
+        },
+        select: {
+            id: true,
+            email: true,
+            firstName: true,
+            lastName: true,
+        }
+    })
+    console.log(user);
+}
+
+async function main() {
+    // await insertUser("user1", "password1", "first1", "last1", "email1", "phone1")
+    // await insertUser("user2", "password2", "first2", "last2", "email2", "phone2")
+    // await insertUser("user3", "password3", "first3", "last3", "email3", "phone3")
+    // await insertUser("user4", "password4", "first4", "last4", "email4", "phone4")
+    // await insertUser("user5", "password5", "first5", "last5", "email5", "phone5")
+
+    // await updateUser("user4", {
+    //     firstName: "dexter",
+    //     lastName: "foo"
+    // })
+
+    await getUser("user4")
+}
+
+main();
