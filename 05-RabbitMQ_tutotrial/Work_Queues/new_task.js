c314b09addec146efa98fb85c3e84426fed03107
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-03-04 11:00:00 +0530

    completed the section 2 - Work Queues

---
+++ 05-RabbitMQ_tutotrial/Work_Queues/new_task.js
@@ -0,0 +1,28 @@
+#!/usr/bin / env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'task_queue';
+        var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true
+        });
+        console.log(" [x] Sent '%s'", msg);
+    });
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
---
+++ 05-RabbitMQ_tutotrial/Work_Queues/worker.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error, connection) {
+    connection.createChannel(function (error, channel) {
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.prefetch(1);
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function (msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function () {
+                console.log(" [x] Done");
+                channel.ack(msg);
+            }, secs * 1000);
+        }, {
+            noAck: false
+        });
+    });
+});
